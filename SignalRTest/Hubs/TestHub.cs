91232dc9ab5153103f69cbc2cd47b5a2f4538f37
redanthrax <redanthraxx@gmail.com>
2021-12-29 10:25:27 -0800

    Initial

---
+++ SignalRTest/Hubs/TestHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRTest.Hubs
+{
+    public class TestHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            Clients.All.SendAsync("ReceiveMessage", "User has connected");
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            Clients.All.SendAsync("ReceiveMessage", "User has disconnected");
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task SendPing()
+        {
+            await Clients.Clients(Context.ConnectionId).SendAsync("ReceivePing", "Received");
+        }
+    }
+}
